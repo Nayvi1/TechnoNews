0408e91f1ff69deab82aea94cc7715d005a0e38b
Hadi <frisk17h@gmail.com>
2023-12-08 17:23:42 +0330

    config fonts and colors

--- tailwind.config.js
+++ tailwind.config.js
@@ -2,7 +2,23 @@
 export default {
   content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        grayFr: "#333",
+        black: "#191919",
+        lighter: "#727272",
+        offWhite: "#E7E6E6",
+        semiGray: "#202425",
+        semiBlack: "#1D1F20",
+        kongFoPanda: "#008170",
+      },
+      fontFamily: {
+        "p-extraLight": "p-extraLight",
+        "p-semiBold": "p-semiBold",
+        "p-regular": "p-regular",
+        "i-medium": "i-medium",
+      },
+    },
   },
   plugins: [],
 };
